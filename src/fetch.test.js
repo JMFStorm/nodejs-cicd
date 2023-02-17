0e0657c8f3dda8416a5aac50f293b128f21520f5
JMFStorm <jannematiasf@gmail.com>
2023-02-17 20:27:10 +0200

    add tests

---
+++ src/fetch.test.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const { FetchPokemonByIndex } = require("./fetch");
+
+const mockPokemon = {
+  name: "Pokemon 1",
+  types: [{ slot: 1, type: { name: "Type name" } }],
+  abilities: [{ ability: { name: "Ability name" } }],
+  weight: 40,
+};
+
+const mockResponse = {
+  data: mockPokemon,
+};
+
+jest.mock("axios");
+axios.get.mockReturnValue(mockResponse);
+
+test("FetchPokemonByIndex returns data", async () => {
+  const index = 1;
+  const result = await FetchPokemonByIndex(index);
+  expect(result).toBeDefined();
+  expect(typeof result).toBe("object");
+});
